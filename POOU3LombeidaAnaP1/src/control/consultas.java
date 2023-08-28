5c6c10ad67f959a6b74fb79b1709b8e16e7af9bd
Lombeida2023 <132691730+Lombeida2023@users.noreply.github.com>
2023-08-28 00:03:15 -0500

    Add files via upload

---
+++ POOU3LombeidaAnaP1/src/control/consultas.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package control;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Usuario
+ */
+public class consultas {
+    public void guardarUsuario(String usuario, String password){
+        Conexion db = new Conexion();
+        String sql = "insert into usuarios(nombre, clave) values ('" + usuario +"', '" + password +"');";
+        Statement st;
+        Connection conexion = db.conexion();
+        try
+        {
+            st = conexion.createStatement();
+            int rs = st.executeUpdate(sql);
+            JOptionPane.showMessageDialog(null, "Guardado correctamente");
+        }catch(SQLException e)
+        {
+            System.out.println(e);
+        }
+    }
+    
+    public void consultarUsuario(String user, String pass)
+    {
+        // TODO add your handling code here:
+        Conexion db = new Conexion();
+        // Se inicializa a null
+        String usuarioCorrecto = null;
+        String passCorrecto = null;
+    try {
+
+        Connection cn = db.conexion();
+        PreparedStatement pst = cn.prepareStatement("SELECT nombre, clave FROM usuarios");
+        ResultSet rs = pst.executeQuery();
+
+        if (rs.next()) {
+            usuarioCorrecto = rs.getString(1);
+            passCorrecto = rs.getString(2);
+        }
+
+        if (user.equals(usuarioCorrecto) && pass.equals(passCorrecto)) {
+            JOptionPane.showMessageDialog(null, "Login correcto Bienvenido " + user);
+        } else if (!user.equals(usuarioCorrecto) || pass.equals(passCorrecto)) {
+
+            JOptionPane.showMessageDialog(null, "Usuario o contraseña incorrectos");
+        }
+
+    } catch (Exception e) {
+        JOptionPane.showMessageDialog(null, "Error " + e);
+    }
+    }
+    
+}
